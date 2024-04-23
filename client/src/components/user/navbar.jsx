982144bfaece69481d2acdae2988c99eedced148
skwizo <bayimostapha@gmail.com>
2024-04-23 16:50:31 +0100

    styled navbar, added font

--- client/src/components/user/navbar.jsx
+++ client/src/components/user/navbar.jsx
@@ -1,24 +1,45 @@
-import { Button } from "@/components/ui/button";
+import { useContext, useState } from "react";
 import { authContext } from "@/contexts/auth-wrapper";
-import { LOGIN, SIGNUP } from "@/router/urls";
-import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-// import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { LOGIN, SIGNUP } from "@/router/urls";
+// shadcn 
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { ChevronDown, LogOut } from "lucide-react";
 
 function NavBar() {
+    const [open, setOpen] = useState(false);
     const { user, logout } = useContext(authContext)
     return (
-        <nav>
-            <h1>Recipes</h1>
+        <nav className="p-3 flex justify-between shadow-sm">
+            <h1 className="text-primary font-bold text-xl">RecipePals</h1>
             {user ? (
-                <div>
-                    {/* <Avatar>
-                        <AvatarImage src="https://github.com/shadcn.png" />
-                        <AvatarFallback>CN</AvatarFallback>
-                    </Avatar> */}
-                    {user.fullname}
-                    <Button variant="outline" onClick={logout}>Log Out</Button>
-                </div>
+                <DropdownMenu open={open} onOpenChange={setOpen}>
+                    <DropdownMenuTrigger className="flex items-center gap-3">
+                        <Avatar>
+                            <AvatarImage src="https://github.com/shadcn.png" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                        <div className="flex flex-col items-start">
+                            <h2 className="text-sm font-semibold">{user.fullname}</h2>
+                            <p className="text-xs">{user.email}</p>
+                        </div>
+                        <ChevronDown className={`transition-all ${open && 'rotate-180'}`} size={16} strokeWidth={1.25} />
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Profile</DropdownMenuItem>
+                        <DropdownMenuItem onClick={logout}>Log Out</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
             ) : (
                 <div>
                     <Link to={SIGNUP}>Sign Up</Link>
